2c7c67add3a9f02ef446d97d3ced4f38ad2de8ed
Sergio Aroca <saroca1993@gmail.com>
2025-03-19 19:43:27 +0100

    feat: Ignore the commented-out part of the code.
    In the configuration file, any content following the # symbol on a line is treated as a comment and ignored during processing. This allows adding inline comments or disabling parts of the configuration without affecting the functionality. The parser reads each line and processes only the part before the #, effectively ignoring anything that comes after it.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,18 @@
 #include "../include/config/HttpBlock.hpp"
 #include "../include/config/LocationBlock.hpp"
 
+bool isFirstMatch(std::size_t const &targetValue, std::size_t const &first,
+    std::size_t const &second, std::size_t const &third)
+{
+    if (targetValue > first)
+        return (false);
+    if (targetValue > second)
+        return (false);
+    if (targetValue > third)
+        return (false);
+    return (true);
+}
+
 void lineBuilder(std::ifstream &filename, std::string &line)
 {
     std::string static buffer;
@@ -24,21 +36,29 @@ void lineBuilder(std::ifstream &filename, std::string &line)
 	std::size_t semicolon = buffer.find(';');
 	std::size_t curlyBracketOpen = buffer.find('{');
 	std::size_t curlyBracketClose = buffer.find('}');
-    if (curlyBracketClose < buffer.size() && curlyBracketClose < curlyBracketOpen && curlyBracketClose < semicolon)
+    std::size_t hash = buffer.find('#');
+    if (curlyBracketClose < buffer.size() && isFirstMatch(curlyBracketClose, curlyBracketOpen, semicolon, hash))
     {
         line = buffer.substr(0, curlyBracketClose + 1);
 		buffer = buffer.substr(curlyBracketClose + 1);
     }
-	else if(semicolon < buffer.size() && (curlyBracketOpen == std::string::npos || semicolon < curlyBracketOpen))
+    else if (curlyBracketOpen < buffer.size() && isFirstMatch(curlyBracketOpen, curlyBracketClose, semicolon, hash))
+    {
+        line = buffer.substr(0, curlyBracketOpen + 1);
+        buffer = buffer.substr(curlyBracketOpen + 1);
+    }
+	else if(semicolon < buffer.size() && isFirstMatch(semicolon, curlyBracketClose, curlyBracketOpen, hash))
 	{
 		line = buffer.substr(0, semicolon + 1);
 		buffer = buffer.substr(semicolon + 1);
 	}
-	else if (curlyBracketOpen < buffer.size() && (semicolon == std::string::npos || curlyBracketOpen < semicolon))
-	{
-		line = buffer.substr(0, curlyBracketOpen + 1);
-		buffer = buffer.substr(curlyBracketOpen + 1);
-	}
+    else if (hash < buffer.size() && isFirstMatch(hash, curlyBracketClose, curlyBracketOpen, semicolon))
+    {
+        line = buffer.substr(0, hash);
+        buffer.clear();
+        std::cout << BLUE << line << WHITE;
+        std::cout << RED << buffer << WHITE;
+    }
 	else
 	{
 		line = buffer;
