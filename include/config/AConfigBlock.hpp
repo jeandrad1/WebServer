a0cbd8a13719fd0eea8f36a89466302017103dd8
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-03-22 19:44:36 +0100

    feat: Add getBlock() method for test purposes

--- include/config/AConfigBlock.hpp
+++ include/config/AConfigBlock.hpp
@@ -9,15 +9,21 @@ class AConfigBlock {
 
 	protected:
 		std::string					_name;
-		std::vector<AConfigBlock *>	blocks;
-	
+		
 		AConfigBlock(void);
 	public:
+		std::vector<AConfigBlock *>	blocks;
 		AConfigBlock(const std::string &name);
 		AConfigBlock(const AConfigBlock &other);
 		virtual	~AConfigBlock(void);
 
 		AConfigBlock	&operator=(const AConfigBlock &other);
+		AConfigBlock	*operator[](int index);
+
+		AConfigBlock	*getBlock(int index)
+		{
+			return (this->blocks[index]);
+		}
 
 		virtual void	printConfig(int indent = 0) const = 0;
 		virtual void	addBlock(AConfigBlock *newBlock);
