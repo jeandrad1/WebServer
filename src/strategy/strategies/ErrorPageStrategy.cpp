0a703206b75c513e7c69f15ff54162c6c69512e3
SalvadorChamizo <salvadorchamizo@gmail.com>
2025-03-24 00:30:06 +0100

    feat: Add error_page validation logic

--- src/strategy/strategies/ErrorPageStrategy.cpp
+++ src/strategy/strategies/ErrorPageStrategy.cpp
@@ -1,7 +1,49 @@
 #include "../../../include/strategy/strategies/ErrorPageStrategy.hpp"
+#include <iostream>
+#include <sstream>
+
+bool isInteger(std::string value)
+{
+    if (value.empty())
+        return (false);
+
+    for (size_t i = 0; i < value.size(); ++i)
+    {
+        if (!isdigit(value[i]))
+            return (false);
+    }
+
+    return (true);
+}
+
+/*bool checkIfCodeIsValid(int code)
+{
+    if (code)
+}*/
 
 bool ErrorPageStrategy::validate(const std::string &value) const
 {
     std::cout << "ErrorPage Strategy. Value: " << value << "\n";
+
+    std::istringstream iss(value);
+
+    while (!iss.eof())
+    {
+        std::string val;
+
+        iss >> val;
+        if (isInteger(val))
+        {
+            int val;
+            iss >> val;
+            std::cout << "Is integer: " << val << "\n";
+        }
+        else
+        {
+            std::string page;
+            iss >> page;
+            std::cout << "Is string: " << page << "\n";
+        }
+    }
     return true;
 }
