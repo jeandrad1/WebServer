be58d738a3f53089da677ff525ffdc1c15a199fa
Sergio Aroca <saroca1993@gmail.com>
2025-03-25 11:40:19 +0100

    feat: add Autoindex strategy
    add Autoindex strategy

---
+++ src/strategy/strategies/AutoindexStrategy.cpp
@@ -0,0 +1,31 @@
+#include "../../../include/strategy/strategies/AutoindexStrategy.hpp"
+#include "../../../include/utils/colors.hpp"
+
+bool AutoindexStrategy::validate(const std::string &value) const
+{
+	if(value.empty())
+		return false;
+	if(value == "on" || value == "off")
+		return true;
+	return false;
+}
+
+/* int main()
+{
+	AutoindexStrategy validator;
+
+	std::string testCases[] = {
+		"",       // Vacío (debe fallar)
+		"on",      // Válido
+		"off",    // Válido
+		"offo",    // Válido
+	};
+
+	for(int i = 0; i < 4; ++i) 
+	{
+		std::cout << "Test case: \"" << testCases[i] << "\" --> " << (validator.validate(testCases[i]) ? GREEN"Valid" : RED"Invalid") << WHITE"\n";
+	}
+
+	return 0;
+} */
+
