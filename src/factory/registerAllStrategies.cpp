16112e8f3d53129c4297952c6fdec3de36c34c80
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-03-26 19:56:05 +0100

    refactor: Comment strategies that doesn't work

--- src/factory/registerAllStrategies.cpp
+++ src/factory/registerAllStrategies.cpp
@@ -7,7 +7,7 @@ void registerAllStrategies(StrategyFactory factory)
     StrategyFactory::getInstance().registerStrategy("autoindex", createAutoIndexStrategy);
     StrategyFactory::getInstance().registerStrategy("client_max_body_size", createClientMaxBodySizeStrategy);
     StrategyFactory::getInstance().registerStrategy("error_page", createErrorPageStrategy);
-    StrategyFactory::getInstance().registerStrategy("index", createIndexStrategy);
-    StrategyFactory::getInstance().registerStrategy("root", createRootStrategy);
-    StrategyFactory::getInstance().registerStrategy("server_name", createServerNameStrategy);
+    //StrategyFactory::getInstance().registerStrategy("index", createIndexStrategy);
+    //StrategyFactory::getInstance().registerStrategy("root", createRootStrategy);
+    //StrategyFactory::getInstance().registerStrategy("server_name", createServerNameStrategy);
 }
