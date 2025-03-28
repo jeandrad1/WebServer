8ec2272e101c94a12384b609c8ba938de9147e5b
SalvadorChamizo <salvadorchamizo@gmail.com>
2025-03-28 19:57:42 +0100

    fix: Fix path to include IValidadtionBlock

--- src/factory/factoryCheck.cpp
+++ src/factory/factoryCheck.cpp
@@ -1,25 +1,24 @@
-#include "../../include/parser/config/AConfigBlock.hpp"
 #include "../../include/strategy/IValidationStrategy.hpp"
-#include "../../include/strategy/IBlockValidationStrategy.hpp"
+#include "../../include/strategy/IValidationStrategyBlock.hpp"
+#include "../../include/parser/config/AConfigBlock.hpp"
 #include "../../include/parser/config/Directive.hpp"
 #include "../../include/factory/StrategyFactory.hpp"
 
-void	factoryCheck(AConfigBlock &config)
+void factoryCheck(AConfigBlock &config)
 {
 	std::vector<AConfigBlock *>::iterator ite = config.end();
 	for (std::vector<AConfigBlock *>::iterator it = config.begin(); it != ite; ++it)
 	{
-		//std::cout << (*it)->getName() << "\n";
 		if (Directive *directive = dynamic_cast<Directive *>(*it))
 		{
-			IValidationStrategy *strategy = StrategyFactory::getInstance().chooseStrategy(dynamic_cast<Directive*>(*it)->getDirective());
+			IValidationStrategy *strategy = StrategyFactory::getInstance().chooseStrategy(dynamic_cast<Directive *>(*it)->getName());
 			if (strategy)
 			{
 				if (!strategy->validate(directive->getValue()))
-					std::cout << "Error detected for directive " << directive->getDirective() << " with value " << directive->getValue() << "\n"; 
+					std::cout << "Error detected for directive " << directive->getName() << " with value " << directive->getValue() << "\n";
 			}
 			else
-				std::cout << "Error: Invalid directive " << directive->getDirective() << "\n";
+				std::cout << "Error: Invalid directive " << directive->getName() << "\n";
 		}
 		else
 		{
