68265ed0bf51dbf3eef7fe274660f35f93072d03
Sergio Aroca <saroca1993@gmail.com>
2025-03-31 18:27:28 +0200

    refactor: refactor createBlockComposite.cpp
    Divide funtion in thre functions.

--- src/parser/composite/createBlocksComposite.cpp
+++ src/parser/composite/createBlocksComposite.cpp
@@ -7,53 +7,64 @@
 
 void buildConfigLine(std::ifstream &filename, std::string &line);
 
+static void processDirective(const std::string &line, AConfigBlock &block)
+{
+	std::istringstream iss(line);
+	std::string key, value;
+	if (iss >> key)
+	{
+		std::size_t semicolon = line.find(";");
+		if (semicolon < line.size())
+		{
+			std::getline(iss, value, ';');
+			value.erase(0, value.find_first_not_of(" \t"));
+			value.push_back(';');
+		}
+		else
+			std::getline(iss, value);
+		block.addBlock(new Directive(key, value));
+	}
+}
+
+static AConfigBlock *createAndAddBlock(const std::string &type, AConfigBlock &block, std::ifstream &filename)
+{
+    AConfigBlock *newBlock = NULL;
+    
+    if (type == "server")
+        newBlock = new ServerBlock(type);
+    else if (type == "http")
+        newBlock = new HttpBlock(type);
+    else if (type == "location")
+        newBlock = new LocationBlock(type);
+    if (newBlock)
+    {
+        block.addBlock(newBlock);
+        createBlock(filename, *newBlock);
+    }
+    return newBlock;
+}
+
 AConfigBlock *createBlock(std::ifstream &filename, AConfigBlock &block)
 {
-    std::string line;
-    buildConfigLine(filename, line);
-    while (!line.empty())
-    {
-        std::size_t http = line.find("http");
-        std::size_t semicolon = line.find(";");
-        if (line.empty()) continue;
-        if (line.find("server") != std::string::npos && line.find("server_name") == std::string::npos)
-        {
-			ServerBlock *server = new ServerBlock("server");
-            block.addBlock(server);
-            createBlock(filename, *server);
-        }
-        else if (http != std::string::npos && http < line.find(" "))
-        {
-			HttpBlock *http = new HttpBlock("http");
-            block.addBlock(http);
-            createBlock(filename, *http);
-        }
-        else if (line.find("location") != std::string::npos)
-        {
-            LocationBlock *location = new LocationBlock("location");
-            block.addBlock(location);
-            createBlock(filename, *location);
-        }
-        else if (line.find("}") != std::string::npos)
+	std::string line;
+	buildConfigLine(filename, line);
+	while (!line.empty())
+	{
+		std::size_t http = line.find("http");
+		std::size_t semicolon = line.find(";");
+		if (line.empty())
+			continue;
+		if (line.find("server") != std::string::npos && line.find("server_name") == std::string::npos)
+			createAndAddBlock("server", block, filename);
+		else if (http != std::string::npos && http < line.find(" "))
+			createAndAddBlock("http", block, filename);
+		else if (line.find("location") != std::string::npos)
+			createAndAddBlock("location", block, filename);
+		else if (line.find("}") != std::string::npos)
 			return (&block);
-        else
-        {
-			std::istringstream iss(line);
-            std::string key, value;
-            if (iss >> key)
-            {
-                if (semicolon < line.size())
-                {
-                    std::getline(iss, value, ';');
-                    value.erase(0, value.find_first_not_of(" \t"));
-                    value.push_back(';');
-                }
-                else
-                    std::getline(iss, value);
-                block.addBlock(new Directive(key, value));
-            }
-        }
+		else
+			processDirective(line, block);
 		buildConfigLine(filename, line);
-    }
-    return (&block);
+	}
+	return (&block);
 }
