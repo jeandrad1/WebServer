dc69c0dd0d6b3de442bae101eb5f518d014e23d0
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-04-14 19:45:22 +0200

    feat: Add handles for ClientMaxBodySize ErrorPage
    I have added the handles for this two directives. The ClientMaxBodySize
    handle multiply the value depending of the letter it has. The errorPage
    handle stores all the status codes and the target.

--- src/builder/HttpBuilder.cpp
+++ src/builder/HttpBuilder.cpp
@@ -4,6 +4,12 @@
 /*                     Constructors & Destructor                       */
 /***********************************************************************/
 
+HttpBuilder::HttpBuilder() : built(false)
+{
+    registerHandler("client_max_body_size", &HttpBuilder::handleClientMaxBodySize);
+    registerHandler("error_page", &HttpBuilder::handleErrorPage);
+}
+
 /***********************************************************************/
 /*                         Operator Overload                           */
 /***********************************************************************/
@@ -11,6 +17,65 @@
 /***********************************************************************/
 /*                          Public Functions                           */
 /***********************************************************************/
+
+void    HttpBuilder::addNestedBuilder(IConfigBuilder *child)
+{
+    ServerConfig *newServer = static_cast<ServerConfig *>(child->build());
+    http->servers.push_back(newServer);
+}
+
+void    HttpBuilder::handleClientMaxBodySize(const std::string &value)
+{
+    if (!value)
+        return ;
+    
+    std::istringstream iss(value);
+    unsigned long   maxBodySize = 0;
+    char            suffix = '\0';
+
+    iss >> maxBodySize;
+    iss >> suffix;
+    switch (tolower(suffix))
+    {
+        case 'k':
+            this->http->clientMaxBodySize = maxBodySize * 1024;
+            break ;
+        case 'm':
+            this->http->clientMaxBodySize = maxBodySize * 1024 * 1024;
+            break ;
+        case 'g':
+            this->http->clientMaxBodySize = maxBodySize * 1024 * 1024 * 1024;
+            break ;
+        case '\0':
+            this->http->clientMaxBodySize = maxBodySize;
+    }
+}
+
+void    HttpBuilder::handleErrorPage(const std::string &value)
+{
+    if (value.empty())
+        return ;
+
+    std::istringstream iss(value);
+    std::vector<std::string> values;
+    std::string info;
+    t_errorPage errorPage;
+
+    while (!iss.eof())
+    {
+        iss >> info;
+        values.push_back(info);
+    }
+    std::vector<std::string>::iterator ite = values.end();
+    ite--;
+    std::string target = *ite;
+    errorPage.target = target.substr(0, target.size() - 1);
+    for (std::vector<std::string>::iterator it = values.begin(); it != ite; it++)
+    {
+        errorPage.statusCodes.push_back(*it);
+    }
+    this->http->errorPages.push_back(errorPage);
+}
 
 /***********************************************************************/
 /*                          Getters & Setters                          */
