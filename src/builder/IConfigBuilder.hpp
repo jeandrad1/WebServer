951d03594a203bd07ccb07e340e9373c8ab53320
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-04-17 18:56:09 +0200

    refactor: Change parameters methods IConfigBuilder
    I have changed some of the methods in IConfigBuilder to match the
    necessities of the project. Now the build method takes an AConfigBlock
    and will be the one who will call all the handles and the
    addNestedBuilder function.
    
    The addNestedBuilder function now takes also the AConfigBlock of the
    current child.
    
    Also I have added a new method called setDefaultValues, which have the
    old functioning of build.

--- src/builder/IConfigBuilder.hpp
+++ src/builder/IConfigBuilder.hpp
@@ -2,6 +2,9 @@
 # define ICONFIGBUILDER_HPP
 
 # include <string>
+# include "../composite/AConfigBlock.hpp"
+# include "../composite/Directive.hpp"
+# include "BuilderDefaultMacros.hpp"
 
 class IConfigBuilder {
     
@@ -9,8 +12,9 @@ class IConfigBuilder {
         virtual         ~IConfigBuilder() {}
 
         virtual void    setDirective(const std::string &key,const std::string &value) = 0;
-        virtual void    addNestedBuilder(IConfigBuilder *child) = 0;
-        virtual void    *build(void) = 0;
+        virtual void    addNestedBuilder(IConfigBuilder *child, AConfigBlock *newBlock) = 0;
+        virtual void    *build(AConfigBlock *block) = 0;
+        virtual void    setDefaultValues(void) = 0;
 };
 
 #endif
