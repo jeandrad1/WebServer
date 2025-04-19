ffff2fc450f13ab247407b41674ad72be0acdaaf
jeandrad1 <jesusandradeperez@gmail.com>
2025-04-19 15:44:25 +0200

    fix: ServerNameStrategy fixed

--- src/strategy/ServerNameStrategy.cpp
+++ src/strategy/ServerNameStrategy.cpp
@@ -1,32 +1,45 @@
 #include "ServerNameStrategy.hpp"
+#include <sstream>
 
 std::string	getSubstringBeforeSemicolon(const std::string &value);
+std::string	trim(const std::string &str);
 
 bool	ServerNameStrategy::validate(const std::string &value) const
 {
-	if (value.size() == 0)
-		return (false);
+    if (value.empty())
+        return (false);
 
-	if (value[value.size() - 1] != ';')
-		return (false);
+    if (value[value.size() - 1] != ';')
+        return (false);
 
-	std::string	real_value = getSubstringBeforeSemicolon(value);
+    std::string real_value = getSubstringBeforeSemicolon(value);
 
-	if (real_value.size() < 1 || real_value.size() > 253)
-		return (false);
+    if (real_value.empty())
+        return (false);
 
-	for (size_t i = 0; i < real_value.size(); ++i)
-	{
-		char c = real_value[i];
-		if (!std::isalnum(static_cast<unsigned char>(c)) && c != '.' && c != '-')
-			return (false);
+    std::istringstream	iss(real_value);
+    std::string 		server_name;
 
-		if (c == '.' && i > 0 && real_value[i - 1] == '.')
-			return (false);
-	}
+    while (iss >> server_name)
+    {
+        server_name = trim(server_name);
 
-	if (real_value[0] == '-' || real_value[value.size() - 1] == '-')
-		return (false);
+        if (server_name.size() < 1 || server_name.size() > 253)
+            return (false);
 
-	return (true);
+        for (size_t i = 0; i < server_name.size(); ++i)
+        {
+            char c = server_name[i];
+            if (!std::isalnum(static_cast<unsigned char>(c)) && c != '.' && c != '-')
+                return (false);
+
+            if (c == '.' && i > 0 && server_name[i - 1] == '.')
+                return (false);
+        }
+
+        if (server_name[0] == '-' || server_name[server_name.size() - 1] == '-')
+            return (false);
+    }
+
+    return (true);
 }
