3ecfc408940addfd555d641fcb150bc565015d45
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-04-26 17:01:04 +0200

    style: Change spaces for tabs and headers
    I have remove innecessary includes and change spaces for tabs in
    HttpBuilder.cpp and HttpBuilder.hpp

--- src/builder/HttpBuilder.cpp
+++ src/builder/HttpBuilder.cpp
@@ -1,5 +1,4 @@
 #include "HttpBuilder.hpp"
-#include <cstdlib>
 
 /***********************************************************************/
 /*                     Constructors & Destructor                       */
@@ -7,10 +6,10 @@
 
 HttpBuilder::HttpBuilder() : built(false), http(new HttpConfig())
 {
-    setDefaultValues();
+	setDefaultValues();
 
-    registerHandler("client_max_body_size", &HttpBuilder::handleClientMaxBodySize);
-    registerHandler("error_page", &HttpBuilder::handleErrorPage);
+	registerHandler("client_max_body_size", &HttpBuilder::handleClientMaxBodySize);
+	registerHandler("error_page", &HttpBuilder::handleErrorPage);
 }
 
 HttpBuilder::~HttpBuilder()
@@ -22,120 +21,116 @@ HttpBuilder::~HttpBuilder()
 /*                          Public Functions                           */
 /***********************************************************************/
 
-void    HttpBuilder::setDirective(const std::string &name, const std::string &value)
+void	HttpBuilder::setDirective(const std::string &name, const std::string &value)
 {
-    dispatchDirective(name, value);
+	dispatchDirective(name, value);
 }
 
-void    HttpBuilder::addNestedBuilder(IConfigBuilder *child, AConfigBlock *newBlock)
+void	HttpBuilder::addNestedBuilder(IConfigBuilder *child, AConfigBlock *newBlock)
 {
-    ServerConfig *newServer = static_cast<ServerConfig *>(child->build(newBlock));
-    http->servers.push_back(newServer);
+	ServerConfig	*newServer = static_cast<ServerConfig *>(child->build(newBlock));
+	http->servers.push_back(newServer);
 }
 
-IConfig    *HttpBuilder::build(AConfigBlock *httpBlock)
+IConfig	*HttpBuilder::build(AConfigBlock *httpBlock)
 {
-    for (AConfigBlock::iterator blockIt = httpBlock->begin(); blockIt != httpBlock->end(); ++blockIt)
-    {
-        if (Directive *directive = dynamic_cast<Directive *>(*blockIt))
-            this->dispatchDirective(directive->getName(), directive->getValue());
-        else
-        {
-            ServerBuilder *serverBuilder = new ServerBuilder();
-            this->addNestedBuilder(serverBuilder, *blockIt);
-            delete serverBuilder;
-        }
-    }
-    this->built = true;
-    return (this->http);
+	for (AConfigBlock::iterator	blockIt = httpBlock->begin(); blockIt != httpBlock->end(); ++blockIt)
+	{
+		if (Directive *directive = dynamic_cast<Directive *>(*blockIt))
+			this->dispatchDirective(directive->getName(), directive->getValue());
+		else
+		{
+			ServerBuilder *serverBuilder = new ServerBuilder();
+			this->addNestedBuilder(serverBuilder, *blockIt);
+			delete (serverBuilder);
+		}
+	}
+	this->built = true;
+	return (this->http);
 }
 
-void    HttpBuilder::setDefaultValues(void)
+void	HttpBuilder::setDefaultValues(void)
 {
-    this->http->clientMaxBodySize = DEFAULT_CLIENT_MAX_BODY_SIZE;
+	this->http->clientMaxBodySize = DEFAULT_CLIENT_MAX_BODY_SIZE;
 }
 
 /***********************************************************************/
-/*                         Operator Overload                           */
+/*                          Handle Functions                           */
 /***********************************************************************/
 
-void    HttpBuilder::handleClientMaxBodySize(const std::string &value)
+void	HttpBuilder::handleClientMaxBodySize(const std::string &value)
 {
-    if (value.empty())
-        return ;
-    
-	std::string real_value = value.substr(0, value.size() - 1);
-    std::istringstream iss(real_value);
-    unsigned long   maxBodySize = 0;
-    char            suffix = '\0';
+	if (value.empty())
+		return ;
 
-    iss >> maxBodySize;
-    iss >> suffix;
+	std::string			real_value = value.substr(0, value.size() - 1);
+	std::istringstream	iss(real_value);
+	unsigned long		maxBodySize = 0;
+	char				suffix = '\0';
 
-    switch (tolower(suffix))
-    {
-        case 'k':
-            this->http->clientMaxBodySize = maxBodySize * 1024;
-            break ;
-        case 'm':
-            this->http->clientMaxBodySize = maxBodySize * 1024 * 1024;
-            break ;
-        case 'g':
-            this->http->clientMaxBodySize = maxBodySize * 1024 * 1024 * 1024;
-            break ;
-        case '\0':
-            this->http->clientMaxBodySize = maxBodySize;
+	iss >> maxBodySize;
+	iss >> suffix;
+
+	switch (tolower(suffix))
+	{
+		case 'k':
+			this->http->clientMaxBodySize = maxBodySize * 1024;
+			break ;
+		case 'm':
+			this->http->clientMaxBodySize = maxBodySize * 1024 * 1024;
+			break ;
+		case 'g':
+			this->http->clientMaxBodySize = maxBodySize * 1024 * 1024 * 1024;
+			break ;
+		case '\0':
+			this->http->clientMaxBodySize = maxBodySize;
 			break ;
 		default :
 			std::cout<<"Impossible value found"<<std::endl;
 			return ;
-    }
-    if (this->http->clientMaxBodySize > (1024 * 1024 * 1024))
+	}
+	if (this->http->clientMaxBodySize > (1024 * 1024 * 1024))
 		this->http->clientMaxBodySize = (1024 * 1024 * 1024);
 }
 
-void    HttpBuilder::handleErrorPage(const std::string &value)
+void	HttpBuilder::handleErrorPage(const std::string &value)
 {
-    std::istringstream iss(value);
-    std::vector<std::string> values;
-    std::string info;
-    t_errorPage *errorPage = new t_errorPage;
-    
-    this->http->errorPageDirective = true;
+	std::istringstream			iss(value);
+	std::vector<std::string>	values;
+	std::string					info;
+	t_errorPage					*errorPage = new t_errorPage;
 
-    if (value.empty())
-    return ;
-    
-    while (!iss.eof())
-    {
-        iss >> info;
-        values.push_back(info);
-    }
-    
-    std::vector<std::string>::iterator ite = values.end();
-    ite--;
-    std::string target = *ite;
+	this->http->errorPageDirective = true;
 
-    errorPage->targetPage = target.substr(0, target.size() - 1);
-    errorPage->isEqualModifier = false;
-    errorPage->equalModifier = 0;
-    errorPage->referencesCount = 0;
+	if (value.empty())
+		return ;
 
-    ite--;
-    if (!(*ite).find('='))
-    {
-        errorPage->isEqualModifier = true;
-        errorPage->equalModifier = std::atol((*ite).substr(1, (*ite).size()).c_str());
-        ite--;
-    }
-    ite++;
-    for (std::vector<std::string>::iterator it = values.begin(); it != ite; it++)
-    {
-        errorPage->referencesCount++;
-        this->http->errorPages[std::atol((*it).c_str())] = errorPage;
-    }
+	while (!iss.eof())
+	{
+		iss >> info;
+		values.push_back(info);
+	}
+
+	std::vector<std::string>::iterator ite = values.end();
+	ite--;
+	std::string target = *ite;
+
+	errorPage->targetPage = target.substr(0, target.size() - 1);
+	errorPage->isEqualModifier = false;
+	errorPage->equalModifier = 0;
+	errorPage->referencesCount = 0;
+
+	ite--;
+	if (!(*ite).find('='))
+	{
+		errorPage->isEqualModifier = true;
+		errorPage->equalModifier = std::atol((*ite).substr(1, (*ite).size()).c_str());
+		ite--;
+	}
+	ite++;
+	for (std::vector<std::string>::iterator it = values.begin(); it != ite; it++)
+	{
+		errorPage->referencesCount++;
+		this->http->errorPages[std::atol((*it).c_str())] = errorPage;
+	}
 }
-
-/***********************************************************************/
-/*                          Getters & Setters                          */
-/***********************************************************************/
