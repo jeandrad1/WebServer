751c0812e531accb8e2488503df389c24d163eb7
saroca <saroca-f@c3r6s5.42malaga.com>
2025-06-24 19:06:41 +0200

    fix: One variable name was wrong

--- assets/www/error_page/error_page.js
+++ assets/www/error_page/error_page.js
@@ -1,6 +1,6 @@
 const params = new URLSearchParams(window.location.search);
 const code = params.get('code') || 'Error';
-const descripcionError = params.get("description_error") || "Ha ocurrido un error.";
+const descripcionError = params.get("descriptionerror") || "Ha ocurrido un error.";
 
-document.getElementById('codigoError').textContent = "Error: " + code;
+document.getElementById('codeError').textContent = "Error: " + code;
 document.getElementById('descripcionError').textContent = descripcionError;
