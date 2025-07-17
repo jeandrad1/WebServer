bb5cc05af8c181a26ba2bcf51962b1c027ad32c2
jeandrad1 <jesusandradeperez@gmail.com>
2025-07-17 12:19:57 +0200

    feat: Added php cgi to execute
    Now the user registration and login is done with php and sets a cookie for the user. Small change to add .php handling in the cgi part. Aslo small changes in the .html to call the .php and added a new unmatched.conf that works as a basic server test.

---
+++ assets/www/cgi-bin/login.php
@@ -0,0 +1,42 @@
+<?php
+$file = '/home/jeandrad/Desktop/webserv/assets/www/register/users.txt';
+
+function verify_user($file, $user, $password) {
+    if (!file_exists($file)) return false;
+    $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        list($existing_user, $hashed_password) = explode(':', $line, 2);
+        if ($existing_user === $user && password_verify($password, $hashed_password)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['user']) && isset($_POST['password'])) {
+    $user = htmlspecialchars(trim($_POST['user']));
+    $password = htmlspecialchars(trim($_POST['password']));
+
+    if (verify_user($file, $user, $password)) {
+        setcookie("user", $user, time() + 3600, "/");
+        echo "Content-Type: text/html\r\n\r\n";
+        echo "<html><body>";
+        echo "<h1>Login exitoso</h1>";
+        echo "<p>Bienvenido, $user.</p>";
+        echo "<a href='index.html'>Ir al inicio</a>";
+        echo "</body></html>";
+    } else {
+        echo "Content-Type: text/html\r\n\r\n";
+        echo "<html><body>";
+        echo "<h1>Usuario o contraseña incorrectos</h1>";
+        echo "<a href='login.html'>Volver</a>";
+        echo "</body></html>";
+    }
+} else {
+    echo "Content-Type: text/html\r\n\r\n";
+    echo "<html><body>";
+    echo "<h1>Error en el login</h1>";
+    echo "<a href='login.html'>Volver</a>";
+    echo "</body></html>";
+}
+?>
